fc8547a81c8f2fbb8d18e7fc0ed46f325b576400
pymonk <pymonk@ex.com>
2018-04-17 10:18:23 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='adhtest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/adarshharidas/adhtest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['adhtest'],
+      zip_safe=False)
